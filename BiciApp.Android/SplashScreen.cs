18d718dd4262bea18ef0b1794fd4e141fb86bfdb
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2023-08-24 00:00:16 -0500

    first commit

---
+++ BiciApp.Android/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BiciApp.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiciApp.Droid
+{
+    [Activity(Label = "BiciApp", Icon = "@mipmap/icoonBike",
+        Theme = "@style/nuevotema",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
---
+++ BiciApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BiciApp.Vistas.Registro;
+
+namespace BiciApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CompletarReg();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
